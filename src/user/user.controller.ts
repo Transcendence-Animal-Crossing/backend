3954ba9805b587f766b97b3dc567ccdce0c14345
aeyongdodam <kikititi0510@gmail.com>
2023-10-11 17:30:35 +0900

    feat: make use find all

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, Param, Req, Res, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ResponseUserDto } from './dto/response-user.dto';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Public } from 'src/auth/guards/public';
 
 @Controller('user')
-@UseGuards(JwtAuthGuard)
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Public()
+  @Get('all')
+  findAll() {
+    return this.userService.findAll();
+  }
 
   @Get(':id')
   findOnyById(@Param('id') id: number): Promise<ResponseUserDto> {
