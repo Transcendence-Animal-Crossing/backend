79acc7e6cde8fa42d412300fefab6b3e262669bb
aeyongdodam <kikititi0510@gmail.com>
2023-10-13 23:38:27 +0900

    feat: make user profile update function

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Res,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { ResponseUserDto } from './dto/response-user.dto';
 import { Public } from 'src/auth/guards/public';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerOptions } from 'src/config/multer.config';
 
 @Controller('user')
 //@UseGuards(JwtAuthGuard)
@@ -18,5 +31,28 @@ export class UserController {
   @Get(':id')
   findOnyById(@Param('id') id: number): Promise<ResponseUserDto> {
     return this.userService.findOneById(id);
+  }
+
+  @Public()
+  @Put('signUp/:id')
+  @UseInterceptors(FileInterceptor('avatar', multerOptions))
+  async firstSignUp(
+    @UploadedFile() file,
+    @Param('id') id: number,
+    @Body('nickName') nickName: string,
+  ) {
+    await this.userService.saveProfileImage(id, nickName, file.filename);
+    return { filepath: 'uploads/' + file.filename };
+  }
+
+  @Public()
+  @Put('signUpWithUrl/:id')
+  async urlSignUp(
+    @Param('id') id: number,
+    @Body('nickName') nickName: string,
+    @Body('avatar') avatar: string,
+  ) {
+    await this.userService.saveUrlImage(id, nickName, avatar);
+    return { filepath: 'original/' + avatar };
   }
 }
