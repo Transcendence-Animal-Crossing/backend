f0ee782fcc9d20496abc680388edf1e27c81aa40
aeyongdodam <kikititi0510@gmail.com>
2023-10-16 01:31:34 +0900

    feat: add game api

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameService } from './game.service';
-import { Game } from './game.entity';
-
+import { Game } from './entities/game.entity';
+import { GameController } from './game.controller';
 @Module({
   imports: [TypeOrmModule.forFeature([Game])],
   providers: [GameService],
+  controllers: [GameController],
   exports: [GameService],
 })
 export class GameModule {}
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { Game } from './game.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../user/entities/user.entity';
+import { Game } from './entities/game.entity';
+import { CreateGameDto } from './dto/create-game.dto';
 
 @Injectable()
 export class GameService {
@@ -18,7 +18,59 @@ export class GameService {
     return this.gameRepository.findOneBy({ id: id });
   }
 
-  async create(game: Game): Promise<Game> {
-    return this.gameRepository.save(game);
+  async create(createGameDto: CreateGameDto) {
+    try {
+      await this.gameRepository.save(createGameDto);
+    } catch (error) {
+      throw new HttpException(
+        'Failed to generate game data',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async findWinGamesById(id: number): Promise<Game[]> {
+    const games = await this.gameRepository
+      .createQueryBuilder('game')
+      .leftJoinAndSelect('game.winner', 'winner')
+      .where('winner.id = :id', {
+        id,
+      })
+      .getMany();
+
+    return games;
+  }
+
+  async findLoseGamesById(id: number): Promise<Game[]> {
+    const games = await this.gameRepository
+      .createQueryBuilder('game')
+      .leftJoinAndSelect('game.loser', 'loser')
+      .where('loser.id = :id', {
+        id,
+      })
+      .getMany();
+
+    return games;
+  }
+  async getAllGamesById(id: number) {
+    const winGames = await this.findWinGamesById(id);
+    const loseGames = await this.findLoseGamesById(id);
+
+    const allGames = [...winGames, ...loseGames].sort((a, b) => {
+      return b.updatedAt.getTime() - a.updatedAt.getTime();
+    });
+
+    const totalGames = allGames.length;
+    const totalWins = winGames.length;
+    const winRate = totalGames === 0 ? 0 : (totalWins / totalGames) * 100;
+
+    return {
+      allGames,
+      stats: {
+        totalGames,
+        totalWins,
+        winRate,
+      },
+    };
   }
 }
