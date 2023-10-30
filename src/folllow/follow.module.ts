7f85207c260eccd3836a856d7c0f370123fc2c61
sseunghoon <sseunghun99@naver.com>
2023-10-30 19:51:34 +0900

    fix: follow.module.ts (add import UserModule)

--- src/folllow/follow.module.ts
+++ src/folllow/follow.module.ts
@@ -5,9 +5,13 @@ import { FollowService } from './follow.service';
 import { FollowController } from './follow.controller';
 import { FollowRequest } from './entities/follow-request.entity';
 import { User } from 'src/user/entities/user.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Follow, FollowRequest, User])],
+  imports: [
+    TypeOrmModule.forFeature([Follow, FollowRequest, User]),
+    UserModule,
+  ],
   providers: [FollowService],
   controllers: [FollowController],
   exports: [FollowService],
