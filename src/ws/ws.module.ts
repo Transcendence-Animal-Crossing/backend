d30d9ed3837e6d437a647d41507a7ce6f613a258
sseunghoon <sseunghun99@naver.com>
2023-11-04 15:07:52 +0900

    refac: ChatModule에 WebSocketModule 추가

---
+++ src/ws/ws.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ClientRepository } from './client.repository';
+import { ClientService } from './client.service';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
+import { RoomModule } from '../room/room.module';
+
+@Module({
+  imports: [AuthModule, UserModule, RoomModule],
+  providers: [ClientRepository, ClientService],
+  exports: [ClientRepository, ClientService],
+})
+export class WebSocketModule {}
