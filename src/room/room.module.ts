633842d278b84fe2deed28cb3f1e9b07442d5cb3
sseunghoon <sseunghun99@naver.com>
2023-11-06 13:20:58 +0900

    fix: 순환 import 해결

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,14 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoomService } from './room.service';
 import { UserModule } from '../user/user.module';
 import { User } from '../user/entities/user.entity';
 import { RoomRepository } from './room.repository';
-import { ClientRepository } from '../ws/client.repository';
+import { WSModule } from '../ws/ws.module';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UserModule],
-  providers: [RoomService, RoomRepository, ClientRepository],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+    forwardRef(() => WSModule),
+    forwardRef(() => ChatModule),
+  ],
+  providers: [RoomService, RoomRepository],
   exports: [RoomService],
 })
 export class RoomModule {}
