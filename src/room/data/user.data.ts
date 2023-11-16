4b85c8171b174f06a07f1ee22c961e3d0194a324
sseunghoon <sseunghun99@naver.com>
2023-11-16 18:56:50 +0900

    del: 필수 id, nickName, intraName, avatar 로 UserData를 만들 수 있도록 변경

--- src/room/data/user.data.ts
+++ src/room/data/user.data.ts
@@ -8,16 +8,31 @@ export class UserData {
     this.intraName = user.intraName;
     this.avatar = user.avatar;
   }
+
   id: number;
   nickName: string;
   intraName: string;
   avatar: string;
 
+  public static create(
+    id: number,
+    nickName: string,
+    intraName: string,
+    avatar: string,
+  ) {
+    const user = new User();
+    user.id = id;
+    user.nickName = nickName;
+    user.intraName = intraName;
+    user.avatar = avatar;
+    return new UserData(user);
+  }
+
   public static from(user: User): UserData {
     return new UserData(user);
   }
 
-  public static compressToParticipant(participant: ParticipantData) {
+  public static compressToParticipant(participant: ParticipantData): UserData {
     return {
       id: participant.id,
       nickName: participant.nickName,
