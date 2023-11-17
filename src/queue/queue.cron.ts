e41af467e56fe23e02adfb27dd58c6a98404a57d
sseunghoon <sseunghun99@naver.com>
2023-11-17 21:12:21 +0900

    feat: Rank Game 매칭 기능 추가

--- src/queue/queue.cron.ts
+++ src/queue/queue.cron.ts
@@ -40,28 +40,83 @@ export class QueueCron {
       .orderBy('standby.createdAt', 'ASC')
       .getMany();
 
-    while (queue.length >= 2) {
-      if (type === GAMETYPE_RANK) await this.rankMatch(manager, queue);
-      else await this.generalMatch(manager, queue);
-    }
+    if (type === GAMETYPE_RANK) await this.rankMatch(manager, queue);
+    else await this.generalMatch(manager, queue);
   }
 
   private async generalMatch(manager: EntityManager, queue) {
     while (queue.length >= 2) {
       const matched = queue.splice(0, 2);
-      const game = await this.gameService.initGame(matched[0].type);
-      for (const user of matched) {
-        const client = await this.clientService.getClientByUserId(
-          Namespace.QUEUE,
-          user.id,
-        );
-        this.queueGateWay.server
-          .to(client)
-          .emit('game-matched', { id: game.id });
-      }
-      await manager.getRepository(Standby).remove(matched);
+      await this.processMatchedUser(manager, matched[0], matched[1]);
     }
   }
 
-  private async rankMatch(manager: EntityManager, queue: ObjectLiteral[]) {}
+  private async rankMatch(manager: EntityManager, queue: ObjectLiteral[]) {
+    queue.sort((a, b) => {
+      return a.rank - b.rank;
+    });
+    this.cacheMatchableRank(queue);
+    while (queue.length >= 2) {
+      const standardUser = queue.shift();
+      await this.findMatchableUser(manager, queue, standardUser);
+    }
+  }
+
+  /**
+   * @description
+   * standardUser가 matchable한 범위를 벗어나면 break
+   * matchable한 범위 안에 들어오면서 standardUser와 matchable한 user를 찾으면 break
+   */
+
+  private async findMatchableUser(
+    manager: EntityManager,
+    queue: ObjectLiteral[],
+    standardUser: ObjectLiteral,
+  ) {
+    for (let i = 0; i < queue.length; ++i) {
+      if (!this.isMatchable(standardUser, queue[i])) break;
+      if (this.isMatchable(queue[i], standardUser)) {
+        const matchedUser = queue.splice(i, 1);
+        --i;
+        await this.processMatchedUser(manager, standardUser, matchedUser);
+        break;
+      }
+    }
+  }
+
+  private async processMatchedUser(
+    manager: EntityManager,
+    userA: ObjectLiteral,
+    userB: ObjectLiteral,
+  ) {
+    const game = await this.gameService.initGame(userA.type);
+    await this.sendMatchedEvent(userA, game);
+    await this.sendMatchedEvent(userB, game);
+    await manager.getRepository(Standby).remove(userA);
+    await manager.getRepository(Standby).remove(userB);
+  }
+
+  private async sendMatchedEvent(user, game) {
+    const client = await this.clientService.getClientByUserId(
+      Namespace.QUEUE,
+      user.id,
+    );
+    this.queueGateWay.server.to(client).emit('game-matched', { id: game.id });
+  }
+
+  private isMatchable(a: ObjectLiteral, b: ObjectLiteral): boolean {
+    return a.matchableMinRank <= b.rank && a.matchableMaxRank >= b.rank;
+  }
+
+  private cacheMatchableRank(queue: ObjectLiteral[]) {
+    for (let i = 0; i < queue.length; ++i) {
+      const matchableDistance = this.matchableDistance(queue[i].createdAt);
+      queue[i].matchableMinRank = queue[i].rank - matchableDistance;
+      queue[i].matchableMaxRank = queue[i].rank + matchableDistance;
+    }
+  }
+
+  private matchableDistance(createdAt: number): number {
+    return 20 + Math.max(1, Math.log2(Date.now() - createdAt) * 10);
+  }
 }
