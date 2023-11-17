3f92bee7bfdea903947cf10e4b9a8979a1f9ac08
sseunghoon <sseunghun99@naver.com>
2023-11-17 18:31:20 +0900

    fix: DM 이 보낸 사람에게도 다시 돌아가도록 변경

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -102,6 +102,12 @@ export class ChatService {
         date: message.created_at,
         text: message.text,
       });
+    client.emit('dm', {
+      id: message.id,
+      senderId: dto.senderId,
+      date: message.created_at,
+      text: message.text,
+    });
   }
 
   async updateLastRead(userId: number, beforeFocus: number) {
