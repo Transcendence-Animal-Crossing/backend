715ce7ac508a229c8895a540ae71479e2184a295
aeyongdodam <kikititi0510@gmail.com>
2023-11-27 21:42:21 +0900

    fix: game loop 수정

--- src/game/game-loop.service.ts
+++ src/game/game-loop.service.ts
@@ -5,6 +5,7 @@ import { OnEvent } from '@nestjs/event-emitter';
 import { GameGateway } from './game.gateway';
 import { GameStatus } from './enum/game.status.enum';
 import { Map } from './enum/map.enum';
+import { MutexManager } from '../mutex/mutex.manager';
 
 @Injectable()
 export class GameLoopService {
@@ -12,6 +13,7 @@ export class GameLoopService {
   constructor(
     private readonly gameGateway: GameGateway,
     private readonly gameRepository: GameRepository,
+    private readonly mutexManager: MutexManager,
   ) {}
 
   @OnEvent('game.start')
@@ -25,39 +27,48 @@ export class GameLoopService {
   }
 
   private async gameLoop(game: Game) {
-    this.logger.debug('<start.game> event is triggered!');
-    if (game.status != GameStatus.PLAYING) return;
-    const collisionSide = game.ball.updatePositionAndCheckCollision(
-      game.players,
-    );
-    if (collisionSide !== null) {
-      game.updateScore(collisionSide);
-      game.ball.init();
-      game.players.init();
-      this.gameGateway.sendEventToGameParticipant(game.id, 'game-score', {
-        left: game.leftScore,
-        right: game.rightScore,
+    await this.mutexManager
+      .getMutex('game' + game.id)
+      .runExclusive(async () => {
+        if (game.status != GameStatus.PLAYING) return;
+        const collisionSide = game.ball.updatePositionAndCheckCollision(
+          //함수 분리해야함
+          game.players,
+        );
+        if (collisionSide !== null) {
+          game.updateScore(collisionSide);
+          game.ball.init();
+          game.players.init();
+          this.gameGateway.sendEventToGameParticipant(game.id, 'game-score', {
+            left: game.leftScore,
+            right: game.rightScore,
+          });
+          await this.gameRepository.update(game);
+          setTimeout(() => {
+            this.gameLoop(game);
+          }, Game.ROUND_INTERVAL);
+          return;
+        }
+        game.players.updatePlayersPosition();
+        game.ball.updateBallPosition();
+        this.gameGateway.sendEventToGameParticipant(game.id, 'game-ball', {
+          x: game.ball.x,
+          y: game.ball.y,
+        });
+        this.gameGateway.sendEventToGameParticipant(game.id, 'game-player', {
+          left: {
+            x: game.players.leftX,
+            y: game.players.leftY,
+          },
+          right: {
+            x: game.players.rightX,
+            y: game.players.rightY,
+          },
+        });
+        this.gameRepository.update(game);
+        setTimeout(() => {
+          this.gameLoop(game);
+        }, 1000 / Map.GAME_FRAME);
       });
-    } else {
-      game.players.updatePlayersPosition();
-      game.ball.updateBallPosition();
-      this.gameGateway.sendEventToGameParticipant(game.id, 'game-ball', {
-        x: game.ball.x,
-        y: game.ball.y,
-      });
-      this.gameGateway.sendEventToGameParticipant(game.id, 'game-player', {
-        left: {
-          x: game.players.leftX,
-          y: game.players.leftY,
-        },
-        right: {
-          x: game.players.rightX,
-          y: game.players.rightY,
-        },
-      });
-    }
-    setTimeout(() => {
-      this.gameLoop(game);
-    }, 1000 / Map.GAME_FRAME);
   }
 }
