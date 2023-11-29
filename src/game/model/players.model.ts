5fc35710d5659299ea1e1b58d7bc462f9c55d40b
aeyongdodam <kikititi0510@gmail.com>
2023-11-29 16:03:05 +0900

    fix: pr 리뷰 내용 반영

--- src/game/model/players.model.ts
+++ src/game/model/players.model.ts
@@ -5,6 +5,14 @@ import { Side } from '../enum/side.enum';
 
 export class Players {
   private static readonly SPEED = 250 / GameSetting.GAME_FRAME;
+  private static readonly LEFT_X_MAX =
+    GameSetting.WIDTH / 2 - GameSetting.THICKNESS - GameSetting.BARRIER;
+  private static readonly RIGHT_X_MIN =
+    GameSetting.WIDTH / 2 + GameSetting.BARRIER;
+
+  private static readonly RIGHT_X_MAX =
+    GameSetting.WIDTH - GameSetting.THICKNESS;
+
   id: string;
 
   leftX: number;
@@ -107,21 +115,12 @@ export class Players {
     this.rightY += this.rightDy;
 
     this.leftX = Math.max(this.leftX, 0);
-    this.leftX = Math.min(
-      this.leftX,
-      GameSetting.WIDTH / 2 - GameSetting.THICKNESS - GameSetting.BARRIER,
-    );
+    this.leftX = Math.min(this.leftX, Players.LEFT_X_MAX);
     this.leftY = Math.max(this.leftY, this.bar);
     this.leftY = Math.min(this.leftY, GameSetting.HEIGHT);
 
-    this.rightX = Math.max(
-      this.rightX,
-      GameSetting.WIDTH / 2 + GameSetting.BARRIER,
-    );
-    this.rightX = Math.min(
-      this.rightX,
-      GameSetting.WIDTH - GameSetting.THICKNESS,
-    );
+    this.rightX = Math.max(this.rightX, Players.RIGHT_X_MIN);
+    this.rightX = Math.min(this.rightX, Players.RIGHT_X_MAX);
     this.rightY = Math.max(this.rightY, this.bar);
     this.rightY = Math.min(this.rightY, GameSetting.HEIGHT);
   }
